e4e238e6712189f74dc5b1a30c5adb099e910b0e
Renz Landicho <renzrulandicho@gmail.com>
2022-11-18 20:46:11 +0800

    add: starting logic for expressions *NEEDS FURTHER TWEAKING!*

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 let numbers = document.querySelectorAll('[data-num]');
+let operators = document.querySelectorAll('[data-op]');
 let displayBottom = document.querySelector('.display-bottom');
 let clearAll = document.querySelector('.AC');
+let result = document.querySelector('.RESULT');
 let displayArray = [];
+let expressArray = [];
 
-numbers.forEach((btn) => {
+
+numbers.forEach(btn => {
   btn.addEventListener('click', () => {
     displayArray.push(btn.innerHTML);
     displayBottom.innerHTML = displayArray.join('');
   });
 });
+
+operators.forEach(btn => {
+  btn.addEventListener('click', () => {
+    transferArray(btn);
+  })
+})
+
+result.addEventListener('click', () => {
+  transferArray(displayArray.join(''));
+  let numberArray = expressArray.join('');
+  console.log(numberArray)
+});
+
+function transferArray(btn) {
+  for (let i = 0; i < displayArray.length; i++) {
+    expressArray.push(displayArray[i]);
+    displayArray.splice(i, 1);
+    i--;
+  }
+  expressArray.push(btn.innerHTML);
+  clearDisplay();
+}
 
 clearAll.addEventListener('click', () => clearDisplay());
 
