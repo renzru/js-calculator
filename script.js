cbfdc2771ecb8bb245b9ae9540e1144d19e2192f
Renz Landicho <renzrulandicho@gmail.com>
2022-11-19 06:03:51 +0800

    add: evaluator function for input

--- script.js
+++ script.js
@@ -3,8 +3,42 @@ let operators = document.querySelectorAll('[data-op]');
 let displayBottom = document.querySelector('.display-bottom');
 let clearAll = document.querySelector('.AC');
 let result = document.querySelector('.RESULT');
-let displayArray = [];
-let expressArray = [];
+let inputArr1 = [];
+let inputArr2 = [];
+let currentOperation;
+
+operators.forEach(btn => {
+  btn.onclick = () => {
+    if (typeof currentOperation !== 'undefined') {
+      let buffer = operate(inputArr1, inputArr2, currentOperation);
+      console.log(buffer)
+      clearArrays();
+      inputArr1.push(buffer);
+    }
+    currentOperation = btn.innerHTML;
+    console.log(currentOperation);
+  }
+})
+
+numbers.forEach(btn => {
+  btn.onclick = () => {
+    if (typeof currentOperation !== 'undefined') {
+      inputArr2.push(btn.innerHTML);
+      console.log(inputArr2);
+      return;
+    }
+    inputArr1.push(btn.innerHTML);
+    console.log(inputArr1);
+  }
+})
+
+function clearArrays() {
+  inputArr1 = [];
+  inputArr2 = [];
+}
+result.onclick = () => {
+
+}
 
 
 function add(a, b) {
@@ -24,10 +58,13 @@ function divide(a, b) {
 }
 
 function operate(a, b, opt) {
+  a = Number(a.join(''));
+  b = Number(b.join(''));
+
   switch (true) {
     case opt === "+":
       return add(a, b);
-    case opt === "-":
+    case opt === "−":
       return subtract(a, b);
     case opt === "×":
       return multiply(a, b);
